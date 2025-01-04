c4c35aa7f966fb14284b1330ef22903fcc349e15
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-01-04 21:18:30 +0100

    feat(main.tf): add AWS resources for VPC setup
    - Added resources for AWS VPC, internet gateway, route table, subnet, security groups, and instance
    - Set up VPC with CIDR block "10.0.0.0/16" and main subnet with CIDR block "10.0.1.0/24"
    - Defined security groups for SSH and HTTP access with corresponding rules
    - Created an AWS instance "bastion" with necessary configurations for VPC setup

--- main.tf
+++ main.tf
@@ -9,9 +9,53 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Main VPC"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id  = aws_vpc.main.id
+
+  tags = {
+    Name = "my-internet-gateway"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Main Subnet"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
 
 resource "tls_private_key" "bastion_key" {
@@ -29,12 +73,42 @@ resource "local_file" "private_key" {
   filename = "bastion_key.pem"
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name   = "Allow SSH"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
 
 resource "aws_instance" "bastion" {
-  ami           = "ami-01816d07b1128cd2d"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.bastion_key_pair.key_name
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  ami                    = "ami-01816d07b1128cd2d"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id              = aws_subnet.main.id
+  key_name               = aws_key_pair.bastion_key_pair.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
+  associate_public_ip_address = true
   tags = {
     Name = "Bastion"
   }
